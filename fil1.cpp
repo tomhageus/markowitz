0d31ff8e18aefe71512db93a8de963a2b60bfaa8
osp <osp@kth.se>
2021-03-21 14:20:06 +0100

    .

--- fil1.cpp
+++ fil1.cpp
@@ -9,26 +9,5 @@ int main(){
 
   int outint = multiplyBy5(9);
   cout  << outint << endl;
-  // void get(char s[], int n){
-  //
-  // }
-  //
-  //
-  //
-  // char a[30];
-  // double sum = 0;
-  // int n= 0;
-  // get(a,30);
-  // while( strlen(a) >0){
-  //   n++;
-  //   sum += strlen(a);
-  //   get(a,30);
-  // }
-  //
-  // put("Antal ord: ");
-  // put(n, 13);
-  // new_line();
-  // put("genomsnitlig längd: ");
-  // put("sum\n", 2, n); new_line();
 
 }
