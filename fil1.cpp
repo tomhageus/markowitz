dccf15c3487baec2f71bb6e34f7337cf27981ba1
osp <osp@kth.se>
2021-03-21 13:25:50 +0100

    experimenterar med egna headfiler

---
+++ fil1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+// testar att inkludera en headerfil som vi själv skrivit
+#include "mf.h"
+
+using namespace std;
+int main(){
+
+  int outint = multiplyBy5(9);
+  cout  << outint << endl;
+  // void get(char s[], int n){
+  //
+  // }
+  //
+  //
+  //
+  // char a[30];
+  // double sum = 0;
+  // int n= 0;
+  // get(a,30);
+  // while( strlen(a) >0){
+  //   n++;
+  //   sum += strlen(a);
+  //   get(a,30);
+  // }
+  //
+  // put("Antal ord: ");
+  // put(n, 13);
+  // new_line();
+  // put("genomsnitlig längd: ");
+  // put("sum\n", 2, n); new_line();
+
+}
---
+++ mf.h
@@ -0,0 +1,8 @@
+// testar att lägga till funktioner som sen används i fil1
+#include <iostream>
+using namespace std;
+
+// multiplicerar en integer med 9
+int multiplyBy5(int a){
+  return a*9;
+}
