1fff030fa4c31679b68c0a2e61f09b30ba24bc0e
osp <osp@kth.se>
2021-03-27 12:45:36 +0100

    data sorting

--- optimizer.cpp
+++ optimizer.cpp
@@ -1,64 +1,44 @@
 #include <iostream>
+#include <list>
+#include <cmath>
 #include <fstream>
-#include <typeinfo>
+#include <vector>
+#include <string>
+
 using namespace std;
 
+// double[][] coVar(double[],double[]){
+//
+// }
 
 
-int readline(){
-  fstream fin;
-  ifstream istream ("Data-Markowitz.csv", ifstream::binary);
-  if(istream){
-    //get length of file
-    istream.seekg(0, istream.end);
-    int length = istream.tellg();
-    istream.seekg (0, istream.beg);
 
-    char * buffer = new char [length];
 
-    cout << "Reading" << length << "characters... ";
+string getAndSortData(string filename){
+  fstream fs{"Data-Markowitz.csv"};
+  string s;
+  string out;
+  // string test;
+  while(fs){
+    // test+= "test";
+    getline(fs,s);
+    out+=(s + "\n\n\n\n");
+    // cout << s<< '\n';
+    // printf("\n\n\n\n");
+    // cout << typeid(s).name() << '\n';
+    // printf("\n\n\n\n");
 
-    istream.read (buffer, length);
-
-    if(istream)
-      cout << "all characters read successfully.";
-    else
-      cout << "error: only " << istream.gcount() << " could be read";
-    for(int k=0; k<100; k++){
-      cout << buffer[k] << endl;
-    }
-    cout << buffer[10] << endl;
-    istream.close();
-    delete[] buffer;
   }
-  return 0;
-  }
-  // fin.read("Data-Markowitz.csv");
-  // string s = typeid(str).name
+  // l = length(s)
+  cout << out ;
 
-  // int rollnum, roll2, count=0;
-  // cout << "Enter the roll number"
-  //      << "Of the student to distreamplay details: ";
-  // cin >> rollnum;
-  //
-  // vector<string> row;
-  // string line, word, temp;
-  //
-  // while(fin >> temp){
-  //   row.clear();
-  //
-  //   getline(fin, line);
-  //
-  //   stringstream s(line);
-  //
-  //   while (getline(, word, ', ')){
-  //
-  //     row.push_back(word)
-  //   }
-  // }
+  return out;
+}
 
 int main(){
-  readline();
-
-
+  string file;
+  cout << "Enter the file containing the stock prices ";
+  cin >> file;
+  string data = getAndSortData(file);
+  cout << data;
 }
